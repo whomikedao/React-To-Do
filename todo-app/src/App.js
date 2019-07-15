736df1726dc28a273304b3df7a43001a7bb0d4d9
Michael Dao <michaeltuandao@gmail.com>
2019-07-15 18:37:42 -0500

    setup init

---
+++ todo-app/src/App.js
@@ -0,0 +1,37 @@
+import React from 'react';
+import './App.css';
+import Todos from './components/Todos';
+
+class App extends Component {
+  state = {
+    todos: [
+      {
+        id: 1,
+        title: 'Take out the trash',
+        completed: false
+      },
+      {
+        id: 2,
+        title: 'eat with the wife',
+        completed: false
+      },
+      {
+        id: 3,
+        title: 'Meeting with boss',
+        completed: true
+      }
+    ]
+  }
+
+
+  render(){
+    console.log(this.state.todos)
+    return (
+      <div className="App">
+        <Todos />
+      </div>
+    );
+  }  
+}
+
+export default App;
